f25935a444c70deb97dbc0e1e8f99fd6305c9402
AdrianOliva2 <aolirio@g.educaand.es>
2023-01-16 09:17:13 +0100

    V2

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const API_KEY = "4c86d86013ec098ba6c4c424bc4c2c03";
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
@@ -6,16 +6,16 @@ const cors = require('cors');
 
 const app = express();
 
-const publicDirectoryPath = path.join(__dirname, '../frontend')
+const publicDirectoryPath = path.join(__dirname, '../frontend');
 
-app.use(cors())
+app.use(cors());
 app.use(express.static(publicDirectoryPath));
 
 app.get('/api/weather', async (req, res) => {
     const loc = req.query.loc;
     const weather = await axios({
         method: 'get',
-        url: `http://api.weatherstack.com/current?access_key=${API_KEY}&query=${loc}`
+        url: `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=${loc}`
     })
     .then(result => {return result.data});
     res.send(weather);
